227eaf359f3c290ebaa3bf29b4525848992899bd
Harish143-dev <saravananharish143@gmail.com>
2025-08-13 12:37:47 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
-// Detect if we are on GitHub Pages
-const repoName = 'e-commerce' // change to your GitHub repo name
 
 export default defineConfig({
-  plugins: [react()],
-  base: process.env.GITHUB_ACTIONS ? `/${repoName}/` : '/', // GitHub Pages → /repo-name/, Netlify → /
+  base: '/',
+  plugins: [react(), tailwindcss(),],
 })
