f32936ed0e93f50e1ff3174d7fdf2033a37e9a67
Harish143-dev <saravananharish143@gmail.com>
2025-08-13 12:13:11 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// Detect if we are on GitHub Pages
+const repoName = 'e-commerce' // change to your GitHub repo name
 
 export default defineConfig({
   plugins: [react()],
-  base: '/e-commerce/'
-});
+  base: process.env.GITHUB_ACTIONS ? `/${repoName}/` : '/', // GitHub Pages → /repo-name/, Netlify → /
+})
